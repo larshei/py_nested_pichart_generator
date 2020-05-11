3f49fb81dd7cb348806717df98cbbc83d9ec813b
Lars Heinrichs <demoniacmilk@web.de>
2020-05-11 23:11:42 +0200

    Refactored

--- run.py
+++ run.py
@@ -2,103 +2,88 @@ import matplotlib.pyplot as plt
 from setup import chart_config as cfg
 from setup import chart_colors
 
-# get total weight (we will scale the sections according to their weight!)
-sections_total_weight = 0
-for section in cfg["sections"]:
-    sections_total_weight += section["weight"]
+def calculate_total_section_weight(sections):
+    sections_total_weight = 0
+    for section in sections:
+        sections_total_weight += section["weight"]
+    return sections_total_weight
 
-# 'sections' are fictional, they just tell us how to scale the elements in each donut diagram.
-number_of_donuts = 0
-for section in cfg["sections"]:
-    donuts_in_section = len(section["layers"])
-    number_of_donuts = max(donuts_in_section,number_of_donuts)
+def calculate_donut_count(sections):
+    number_of_donuts = 0
+    for section in sections:
+        donuts_in_section = len(section["donuts"])
+        number_of_donuts  = max(donuts_in_section, number_of_donuts)
+    return number_of_donuts
 
-# width per element in each donuts section
-donuts = dict()
-donuts["sizes"] = []
-donuts["labels"] = []
-donuts["element_colors"] = []
+def calculate_donut_section_weight(section, donut_number):
+    total_weight = 0
+    element_list = section["donuts"][donut_number]["elements"]
+    for element in range(0, len(element_list)):
+        total_weight = total_weight + element_list[element]
+    return total_weight
 
-colors = [
-    chart_colors["red"],
-    chart_colors["purple"],
-    chart_colors["blue"],
-    chart_colors["cyan"],
-    chart_colors["cyan5"],
-    chart_colors["cyan2"]
-]
 
-# go through donuts.
-# for each donut, check the elements and scale them.
-# store the scaled elements sizes of all sections for this donut
-for donut_number in range(0, number_of_donuts):
+def generate_donut_elements(sections, donut_number, sections_total_weight):
     element_sizes  = []
     element_colors = []
     element_labels = []
-    for section in cfg["sections"]:
-        donuts_in_section = len(section["layers"])
-        donut_in_section_not_empty = (donuts_in_section >= donut_number + 1)
-        section_size = section["weight"] / sections_total_weight
-        if donut_in_section_not_empty:
-            elements_in_section_donut = len(section["layers"][donut_number]["elements"])
+    donut_color   = chart_colors[donut_number]
+
+    for section in sections:
+        donuts_in_section         = len(section["donuts"])
+        donut_in_section_is_empty = (donuts_in_section <= donut_number)
+        section_size              = section["weight"] / sections_total_weight
+        section_color             = chart_colors[sections.index(section)]
+
+        if donut_in_section_is_empty:
+            element_sizes.append(section_size)
+            element_colors.append(empty_color)
         else:
-            elements_in_section_donut = 1
-
-        for element_number in range(0, elements_in_section_donut):
-            if cfg["color_by_ring"]:
-                color = colors[donut_number]#(color_saturation)
-            else:
-                color = colors[cfg["sections"].index(section)]#(color_saturation)
-
-            if donut_in_section_not_empty:
-                label = section["layers"][donut_number]["elements"][element_number]
-            else:
-                color = (0,0,0,0)
-                label = None
-
-            element_sizes.append(section_size / elements_in_section_donut)
-            element_colors.append(color)
-            element_labels.append(label)
-            
-
-    donuts["sizes"].append(element_sizes)
-    donuts["labels"].append(element_labels)
-    donuts["element_colors"].append(element_colors)
+            elements_in_donut_section = len(section["donuts"][donut_number]["elements"])
+            element_total_weight = calculate_donut_section_weight(section, donut_number)
+            for element in section["donuts"][donut_number]["elements"]:
+                element_color = donut_color if cfg["color_by_ring"] else section_color
+                element_size  = section_size * element / element_total_weight
+                element_sizes.append(element_size)
+                element_colors.append(element_color)
+    return (element_sizes, element_colors)
 
 
-
-#create drawarea
-colors=[plt.cm.Blues, plt.cm.Reds, plt.cm.Greens, plt.cm.Purples, plt.cm.Oranges]
-fig, ax = plt.subplots()
-ax.axis('equal')
-
-donut_drawings = [None] * number_of_donuts
-donut_radius =0
-for donut_number in range(0, number_of_donuts):
-    donut_radius =  donut_radius + 0.8 / (donut_number + 1) + 0.2
+def add_donut_to_plot(axis, donuts, donut_number, donut_radius):
     donut_radius_next =  donut_radius + 0.8 / (donut_number + 1) + 0.2
     donut_width = donut_radius_next - donut_radius
-    color_val = 0.6 - donut_number * 0.1
-    print(donuts["element_colors"][donut_number])
-    label_distance = (donut_radius - donut_width/2) / donut_radius
-    labels =  donuts["labels"][donut_number] if cfg["show_labels"] else None  
-    donut_drawings[donut_number], _ = ax.pie(
-                                            donuts["sizes"][donut_number], 
-                                            labels = labels, 
-                                            labeldistance = label_distance,
-                                            radius = donut_radius, 
-                                            startangle = cfg["start_angle"],
-                                            colors = donuts["element_colors"][donut_number] )
-    plt.setp( donut_drawings[donut_number], width = donut_radius_next - donut_radius, edgecolor='white')
- 
-# # First Ring (outside)
+    donut_drawing, _ = axis.pie(
+                        donuts["element_sizes"][donut_number],
+                        radius = donut_radius, 
+                        startangle = cfg["start_angle"],
+                        colors = donuts["element_colors"][donut_number]
+                    )
+    return donut_drawing, donut_width
 
-# mypie, _ = ax.pie(group_size, radius=1.3, labels=group_names, colors=[a(0.6), b(0.6), c(0.6)] )
- 
-# # Second Ring (Inside)
-# mypie2, _ = ax.pie(subgroup_size, radius=1.3-0.3, labels=subgroup_names, labeldistance=0.7, colors=[a(0.5), a(0.4), a(0.3), b(0.5), b(0.4), c(0.6), c(0.5), c(0.4), c(0.3), c(0.2)])
-# plt.setp( mypie2, width=0.4, edgecolor='white')
-# plt.margins(20,20)
+if __name__ == "__main__":
+    fig, ax      = plt.subplots()
+    ax.axis('equal')
+    donut_radius = 0
+    colors       = chart_colors
+    empty_color  = (0,0,0,0)
+    donuts       = dict()
+    donuts["element_sizes"]  = []
+    donuts["element_colors"] = []
 
-# # show it
-plt.show()
+    number_of_donuts     = calculate_donut_count(cfg["sections"])
+    total_section_weight = calculate_total_section_weight(cfg["sections"])
+
+    for donut_number in range(0, number_of_donuts):
+        element_sizes, element_colors = generate_donut_elements(
+                                            cfg["sections"], 
+                                            donut_number, 
+                                            total_section_weight
+                                            )
+        donuts["element_sizes" ].append(element_sizes)
+        donuts["element_colors"].append(element_colors)
+
+        donut_radius = donut_radius + 0.8 / (donut_number + 1) + 0.2
+        donut_drawing, donut_width = add_donut_to_plot(ax, donuts, donut_number, donut_radius)
+        plt.setp( donut_drawing, width = donut_width, edgecolor = 'white')
+
+    plt.show()
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-chart_colors = {
-    "red"   : '#FF0613FF',
-    "purple": '#AA0061FF',
-    "blue"  : '#020A85FF',
-    "cyan"  : '#469DDDFF',
-    "cyan5" : '#469DDD7F',
-    "cyan2" : '#469DDD33',
-}
+chart_colors = (
+    '#FF0613FF',
+    '#AA0061FF',
+    '#020A85FF',
+    '#469DDDFF',
+    '#469DDD7F',
+    '#469DDD33',
+)
 
 chart_config = {
     "scale_for_highest_element_ring" : False,
@@ -15,42 +15,42 @@ chart_config = {
 
     "sections" : [
         {
-            "name": "Section 1",
+            "name": "Embedded",
             "weight" : 1,
-            "layers": [
-                { "elements": [ "eSDK" ] },
-                { "elements": [ "HW Module", "HW Blueprint", "SDK Integration Support" ] },
-                { "elements": [ "HW Development", "SW Development" ] },
-                { "elements": [ "Embedded Product Development" ] },
+            "donuts": [
+                { "elements": [ 1 ] },
+                { "elements": [ 1, 1, 1 ] },
+                { "elements": [ 1, 1 ] },
+                { "elements": [ 1 ] },
             ]
         },
         {
-            "name": "Section 2",
+            "name": "Mobile",
             "weight" : 1,
-            "layers": [
-                { "elements": [ "mSDK" ] },
-                { "elements": [ "Whitelabel App", "SDK Integration Support" ] },
-                { "elements": [ "Mobile\nApp\nDevelopment" ] },
+            "donuts": [
+                { "elements": [ 1 ] },
+                { "elements": [ 1, 1 ] },
+                { "elements": [ 1 ] },
             ]
         },
         {
-            "name": "Section 3",
+            "name": "Commissioning",
             "weight" : 1,
-            "layers": [
-                { "elements": [ "Commision\nAPI" ] },
-                { "elements": [ "Integration Support", "Asset Management Database" ] },
-                { "elements": [ "Whitelabel Asset\nManagement App/web" ] },
-                { "elements": [ "Permission Management Development" ] },
+            "donuts": [
+                { "elements": [ 1 ] },
+                { "elements": [ 1, 1 ] },
+                { "elements": [ 1 ] },
+                { "elements": [ 1 ] },
             ]
         },
         {
-            "name": "Section 4",
+            "name": "Service",
             "weight" : 1,
-            "layers": [
-                { "elements": [ "Service API" ] },
-                { "elements": [ "Integration Support", "ID Management" ] },
-                { "elements": [ "Whitelabel User\nManagement App/Web" ] },
-                { "elements": [ "Permission Management Development" ] },
+            "donuts": [
+                { "elements": [ 1 ] },
+                { "elements": [ 1, 1 ] },
+                { "elements": [ 1 ] },
+                { "elements": [ 1 ] },
             ]
         }
     ]
